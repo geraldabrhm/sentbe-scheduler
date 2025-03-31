059a798f954a20930dd03fe9b36dd17a86d753bb
geraldabrhm <gerald.sianturi@dana.id>
2025-03-31 16:04:41 +0700

    feat: Adjust notification timing for improved user experience

--- constants/scheduler.ts
+++ constants/scheduler.ts
@@ -1,6 +1,6 @@
-const PUSH_NOTIF_TIME = 10; // range from 0 to 23
-const PUSH_NOTIF_TIME_HHMMSS = "10:26:00";
-const QUERY_USER_BIRTHDAY_AGENDA_INTERVAL = "25 3 * * *"; // every day at 00:00
+const PUSH_NOTIF_TIME = 9; // range from 0 to 23
+const PUSH_NOTIF_TIME_HHMMSS = "09:00:00";
+const QUERY_USER_BIRTHDAY_AGENDA_INTERVAL = "0 0 * * *"; // every day at 00:00
 const MILLISECOND_IN_A_DAY = 24 * 60 * 60 * 1000;
 
 
